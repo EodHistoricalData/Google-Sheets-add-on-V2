cb03ae7d384d076226629dd44974ee9ccade2dab
mineralka-yakudik <yakudik@gmail.com>
2024-05-31 01:14:07 +0300

    fix: fix masked input clean process

--- src/scripts/components/input-mask.js
+++ src/scripts/components/input-mask.js
@@ -1,9 +1,11 @@
-import { formatDate } from 'cleave-zen'
+import { formatDate, registerCursorTracker } from 'cleave-zen'
 
 export function initMask(elem) {
     const maskType = elem.dataset.mask
 
     if (maskType === 'date') {
+        registerCursorTracker({ input: elem, delimiter: '/' })
+
         elem.addEventListener('input', (e) => {
             elem.value = formatDate(e.target.value, { datePattern: ['d', 'm', 'Y']})
         })
